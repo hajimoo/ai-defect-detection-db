e3ec7bb036e87e8244fffb8c5f6830ae91ff9414
shin jaewon <itrust67709901@gmail.com>
2026-03-25 02:46:55 +0900

    Update views.sql

--- sql/views.sql
+++ sql/views.sql
@@ -1,13 +1,25 @@
-CREATE VIEW vw_prediction_summary AS
+USE ai_defect_detection;
+
+CREATE OR REPLACE VIEW vw_prediction_summary AS
 SELECT
-    p.id,
+    p.id AS prediction_id,
     p.label,
     p.confidence,
     p.status,
-    u.email,
+    p.model_version,
+    p.is_low_confidence,
+    p.failure_reason,
+    p.similar_case_group,
+    p.created_at AS prediction_created_at,
+    u.email AS user_email,
     ui.original_filename,
+    ui.stored_path,
     rq.review_status,
-    dc.is_dataset_candidate
+    rq.human_label,
+    rq.ai_review_summary,
+    dc.is_dataset_candidate,
+    dc.candidate_reason,
+    dc.candidate_status
 FROM predictions p
 JOIN users u ON p.user_id = u.id
 JOIN uploaded_images ui ON p.image_id = ui.id
